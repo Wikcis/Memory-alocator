29a0a6664ef72b536e9c574a76ac1126adceab6b
Wikcis <wiktor.ciszewski7734@gmail.com>
2023-07-14 15:44:53 +0200

    Refactor main.c file

--- main.c
+++ main.c
@@ -2,28 +2,38 @@
 #include "heap.h"
 
 int main(void) {
+
     heap_setup();
-    char *ptr[100];
+
+    char *ptr[10];
+
     for(int i = 0; i<5; i++)
     {
-        ptr[i] = heap_malloc(123);
+        ptr[i] = heap_malloc(50);
     }
-    heap_free(ptr[0]);
-    heap_free(ptr[3]);
     heap_free(ptr[1]);
-    ptr[6] = heap_malloc_aligned(9538);
-    ptr[7] = heap_malloc_aligned(9538);
-    if(((intptr_t)ptr[6] & (intptr_t)(PAGE_SIZE - 1)) == 0)
-    {
-        printf("jd33");
+    heap_free(ptr[4]);
+    heap_free(ptr[2]);
+    
+    ptr[5] = heap_malloc_aligned(2254);
+    ptr[6] = heap_malloc_aligned(8000);
+    ptr[7] = heap_malloc_aligned(12000);
+
+    heap_free(ptr[6]);
+
+    if(((intptr_t)ptr[5] & (intptr_t)(PAGE_SIZE - 1)) == 0){
+        printf("12 least significant bits of the address in ptr are set to zero\n");
     }
 
     printHeap();
+
     int status = heap_validate();
-    if(status == 1 || status == 2 || status == 3)
-    {
+    
+    if(status == 1 || status == 2 || status == 3){
+        printf("Heap is invalid\n");
         return -1;
     }
     return 0;
 }
+
 
